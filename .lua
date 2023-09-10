af065bec47be0bf38284cb201837c6d65f71a042
Ambatukam <117529823+Sidhsksjsjsh@users.noreply.github.com>
2023-09-10 09:22:48 +0900

    Update .lua

--- .lua
+++ .lua
@@ -1,14 +1,15 @@
-local ReturningBlocksName
+local ReturningBlocksName = nil
 local mt = getrawmetatable(game);
 setreadonly(mt,false)
 local namecall = mt.__namecall
+local workspace = game:GetService("Workspace")
 
 mt.__namecall = newcclosure(function(self, ...)
 	local Method = getnamecallmethod()
 	local Args = {...}
 
-	if Method == 'InvokeServer' and self.Name == 'purchaseEgg' then
-        DeleteFuckingPet = Args[2]
+	if Method == 'InvokeServer' and self.Name == 'DataPullFunc' and Args[2] == workspace:find("Mine") or workspace:FindFirstChild("Mine") then
+        ReturningBlocksName = Args[2]
 end
 	return namecall(self, ...) 
 end)
@@ -21,3 +22,9 @@ local Tab1 = Window_1:NewSection("Main")
 
 
 Tab1:CreateToggle("Auto Click Block", function(value)
+_G.ClickDick = value
+	while wait() do
+	     if _G.ClickDick == false then break end
+		game:GetService("ReplicatedStorage").CommonLibrary.Tool.RemoteManager.Funcs.DataPullFunc:InvokeServer("CalculatePlayerDamageChannel",ReturningBlocksName)
+	end
+end)
